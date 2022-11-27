51e836427be74586a012c1b2031308c95f60964d
Den <52295780+josserden@users.noreply.github.com>
2022-11-27 13:06:42 +0200

    finish task 3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,15 +6,16 @@ import {
   Statistics,
   ForbesList,
   CryptoHistory,
-} from "components";
-import article from "data/article.json";
-import data from "data/data.json";
+} from 'components';
+import article from 'data/article.json';
+import data from 'data/data.json';
+import forbes from 'data/forbes.json';
 
 export const App = () => {
   return (
     <Section>
       <Container>
-        <Heading marginBottom="50px" textAlign="center">
+        <Heading marginBottom='50px' textAlign='center'>
           Task 1
         </Heading>
         {/* <BlogCard
@@ -27,16 +28,18 @@ export const App = () => {
           postedAt={article.postedAt}
         /> */}
         <BlogCard {...article} />
-        <Heading marginTop="50px" marginBottom="50px" textAlign="center">
+
+        <Heading marginTop='50px' marginBottom='50px' textAlign='center'>
           Task 2
         </Heading>
-        <Statistics title="Main Statistics" stats={data} />
+        <Statistics title='Main Statistics' stats={data} />
 
-        <Heading marginTop="50px" marginBottom="50px" textAlign="center">
+        <Heading marginTop='50px' marginBottom='50px' textAlign='center'>
           Task 3
         </Heading>
-        <ForbesList />
-        <Heading marginTop="50px" marginBottom="50px" textAlign="center">
+        <ForbesList list={forbes} />
+
+        <Heading marginTop='50px' marginBottom='50px' textAlign='center'>
           Task 4
         </Heading>
         <CryptoHistory />
--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -8,6 +8,19 @@ import {
   LeaderBoardProfiles,
 } from './ForbesList.styled';
 
-export const ForbesList = () => {
-  return <div>ForbesList</div>;
+export const ForbesList = ({ list }) => {
+  return (
+    <LeaderBoard>
+      <BoardHeader>
+        <BoardTitle>
+          <TitleTop>Forbes</TitleTop>
+          <TitleBottom>Leader board</TitleBottom>
+        </BoardTitle>
+      </BoardHeader>
+
+      <LeaderBoardProfiles>
+        {list && list.map((item) => <ForbesListItem key={item.id} {...item} />)}
+      </LeaderBoardProfiles>
+    </LeaderBoard>
+  );
 };
